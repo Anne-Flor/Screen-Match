3316a8b148852244107395cad2835440feaa235c
Flor <annygabrielly730@gmail.com>
2024-03-30 12:41:46 -0300

    Criando métodos

--- Filme.java
+++ Filme.java
@@ -2,8 +2,22 @@ public class Filme {
     String titulo;              //modelo
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
+    void exibeFichaTecnica(){ //criando o método
+        System.out.println("Nome do filme : " + titulo);
+        System.out.println("Ano de lançamento " + anoDeLancamento);
+        System.out.println("Duração : " + duracaoEmMinutos);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,11 +1,17 @@
 public class Principal {
     public static void main(String[] args){
-        Filme tituloFilme1 = new Filme();
+        Filme tituloFilme1 = new Filme(); //new para criar  instancia do objeto
         tituloFilme1.titulo = "O poderoso chefão";
         tituloFilme1.anoDeLancamento = 1970;
         tituloFilme1.duracaoEmMinutos = 180;
 
-        System.out.println(tituloFilme1.titulo);
+        tituloFilme1.exibeFichaTecnica();
+        tituloFilme1.avalia(8);
+        tituloFilme1.avalia(5);
+        tituloFilme1.avalia(10);
+        System.out.println(tituloFilme1.somaDasAvaliacoes);
+        System.out.println(tituloFilme1.totalDeAvaliacoes);
+        System.out.println(tituloFilme1.pegaMedia());
 
     }
 }
